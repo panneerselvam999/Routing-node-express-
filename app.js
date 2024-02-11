d297801263811754b4101131099820404acd4405
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-02-11 10:12:04 -0500

    connect style.css file

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080; // set the port
 const pagesRoutes = require("./routes/pages");
+const rootDir = require("./utils/path");
 
 const path = require("path");
 
 app.use(pagesRoutes);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on : ${port}`);
--- routes/pages.js
+++ routes/pages.js
@@ -1,36 +1,40 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const rootDir = require("../utils/path");
 
 // router.use("/product", (req, res, next) => {
-//   res.send("<h1>product</h1>");
+//   res.sendFile(path.join(__dirname, "..", "views", "product.html"));
 // });
 // router.use("/buy", (req, res, next) => {
-//   res.send("<h1>buy</h1>");
+//   res.sendFile(path.join(__dirname, "..", "views", "buy.html"));
 // });
 // router.use("/contact", (req, res, next) => {
-//   res.send("<h1>contact</h1>");
+//   res.sendFile(path.join(__dirname, "..", "views", "contact.html"));
 // });
 // router.get("/", (req, res, next) => {
-//   res.send("<h1>HOME</h1>");
+//   res.sendFile(path.join(__dirname, "..", "views", "home.html"));
 // });
 // router.use((req, res, next) => {
-//   res.status(404).send("<h1>404</h1>");
+//   res.sendFile(path.join(__dirname, "..", "views", "404.html"));
 // });
 router.use("/product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "views", "product.html"));
+  res.sendFile(path.join(rootDir, "views", "product.html"));
 });
 router.use("/buy", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "views", "buy.html"));
+  res.sendFile(path.join(rootDir, "views", "buy.html"));
 });
 router.use("/contact", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "views", "contact.html"));
+  res.sendFile(path.join(rootDir, "views", "contact.html"));
 });
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "views", "home.html"));
+  res.sendFile(path.join(rootDir, "views", "home.html"));
 });
-router.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "views", "404.html"));
-});
+
+// router.use(express.static(path.join(__dirname, "public")));
+
+// router.use((req, res, next) => {
+//   res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+// });
 
 module.exports = router;

80cbe45a0179aaf3ccaa33b9b87eddb5736083ac
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-02-11 05:35:44 -0500

    route html file

--- app.js
+++ app.js
@@ -3,23 +3,9 @@ const app = express();
 const port = process.env.PORT || 8080; // set the port
 const pagesRoutes = require("./routes/pages");
 
-app.use(pagesRoutes);
+const path = require("path");
 
-// app.use("/product", (req, res, next) => {
-//   res.send("<h1>product</h1>");
-// });
-// app.use("/buy", (_req, res, next) => {
-//   res.send("<h1>buy</h1>");
-// });
-// app.use("/contact", (req, res, next) => {
-//   res.send("<h1>contact</h1>");
-// });
-// app.get("/", (req, res, next) => {
-//   res.send("<h1>HOME</h1>");
-// });
-app.use((req, res, next) => {
-  res.status(404).send("<h1>404</h1>");
-});
+app.use(pagesRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on : ${port}`);
--- routes/pages.js
+++ routes/pages.js
@@ -1,17 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 
+// router.use("/product", (req, res, next) => {
+//   res.send("<h1>product</h1>");
+// });
+// router.use("/buy", (req, res, next) => {
+//   res.send("<h1>buy</h1>");
+// });
+// router.use("/contact", (req, res, next) => {
+//   res.send("<h1>contact</h1>");
+// });
+// router.get("/", (req, res, next) => {
+//   res.send("<h1>HOME</h1>");
+// });
+// router.use((req, res, next) => {
+//   res.status(404).send("<h1>404</h1>");
+// });
 router.use("/product", (req, res, next) => {
-  res.send("<h1>product</h1>");
+  res.sendFile(path.join(__dirname, "..", "views", "product.html"));
 });
 router.use("/buy", (req, res, next) => {
-  res.send("<h1>buy</h1>");
+  res.sendFile(path.join(__dirname, "..", "views", "buy.html"));
 });
 router.use("/contact", (req, res, next) => {
-  res.send("<h1>contact</h1>");
+  res.sendFile(path.join(__dirname, "..", "views", "contact.html"));
 });
 router.get("/", (req, res, next) => {
-  res.send("<h1>HOME</h1>");
+  res.sendFile(path.join(__dirname, "..", "views", "home.html"));
+});
+router.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "views", "404.html"));
 });
 
 module.exports = router;

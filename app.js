ba9a5263033b8d8176580e935aec8f63e4cc7474
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-02-10 21:52:50 -0500

    middlware

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080; // set the port
+
+app.use("/product", (req, res, next) => {
+  res.send("<h1>product</h1>");
+});
+app.use("/buy", (req, res, next) => {
+  res.send("<h1>buy</h1>");
+});
+app.use("/contact", (req, res, next) => {
+  res.send("<h1>contact</h1>");
+});
+app.get("/", (req, res, next) => {
+  res.send("<h1>HOME</h1>");
+});
+app.use((req, res, next) => {
+  res.status(404).send("<h1>404</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on : ${port}`);
+});
